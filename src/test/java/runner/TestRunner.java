514b0132e4ec70e30e02a62dede0b2fad7cdc1e6
Santosh Subramanya <santosh.subramanya@sympli.com.au>
2023-07-04 11:37:30 +1000

    updated tests

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/search.feature",
+        features = "src/test/resources/features/Search.feature",
         glue = {"stepDefinitions",
                 "hooks",
                 "drivers"},
